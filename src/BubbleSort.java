d3a456959c3c99e3ed630ff6d8d77bb5c616b63e
hoangnhat4280 <nhatduong3002@gmail.com>
2024-11-19 16:06:17 +0700

    sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,27 @@
+
+public class BubbleSort {
+    static int[] list = {2, 8, 6, 3, 4, 16, 12, 11, 14};
+
+    public static void bubbleSort(int[] list) {
+        boolean needNextPass = true;
+
+        for (int j = 1; j < list.length && needNextPass; j++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length - j; i++) {
+                if (list[i] > list[i + 1]) {
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+}
